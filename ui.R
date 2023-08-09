b348c4c28bc310e02ce58c0073e81a21fc7c763b
Amanda D. Clark <adc0032@auburn.edu>
2023-08-09 11:17:08 -0500

    removed extra commas and added missing parentheses to deploy.

--- ui.R
+++ ui.R
@@ -46,7 +46,7 @@ ui <- fluidPage(
         tags$img(
           src = "overview.png",
           width = "750px",
-          height = "350px",
+          height = "350px"
         )
       )
     ),
@@ -61,7 +61,7 @@ ui <- fluidPage(
         ),
         tags$li(
           "TPM data was combined with prior knowledge from",
-          a(href = "https://github.com/saezlab/CollecTRI", "CollecTRI.", target = "_blank"),
+          a(href = "https://github.com/saezlab/CollecTRI", "CollecTRI.", target = "_blank")
         ),
         tags$li(
           "We used a multivariate linear model (run_mlm) implemented in", 
@@ -70,57 +70,57 @@ ui <- fluidPage(
           
         ),
         tags$li(
-          "We scaled and centered data prior to summarizing the averaged regulator activity for each TF.",
+          "We scaled and centered data prior to summarizing the averaged regulator activity for each TF."
         ),
         tags$li(
           "TF activity scores are represented on a scale from positive to negative corresponding to activator and repressor TF roles, respectively."
-          ,         
-      ),
-      br(),
-      p(
-        "For more information about GTEx, visit their",
-        a(href = "https://www.gtexportal.org/home/", "website", target = "_blank")
+        ),
+        br(),
+        p(
+          "For more information about GTEx, visit their",
+          a(href = "https://www.gtexportal.org/home/", "website", target = "_blank")
+        )
       )
-    )
-  ),  # Closing parenthesis added here to close the div properly
-  br(),
-  br(),
-  hr(),
-  div(
-    class = "instruct",
-    p(
-      strong("Instructions:"),
-      br(),
-      "1. Select one or more TFs from the list.**",
-      br(),
-      "2. Click the 'Plot Heatmap' button to generate the visualization.",
-      br(),
-      br(),
-      strong("**We recommend visualizing less than 30 TFs at one time for readability."),
-      br(),
-      em("Note: The full dataset of activity scores can be found",
-         a(href = "https://github.com/lasseignelab/230323_JW_DiseaseNetworks/blob/main/results/tf_activity/tf_activity_scores.csv", "here", target = "_blank"))
-    )
-  ),
-  
-  # First fluid row for input elements
-  fluidRow(
-    column(
-      width = 4, # Adjust the width to your preference for input elements
-      # Input elements here (e.g., selectInput, actionButton, etc.)
-      selectInput("tf_input_select", "Select TF(s):", choices = all_tfs, multiple = TRUE),
-      actionButton("plot_button", "Plot Heatmap")
-    )
-  ),
-  
-  # Second fluid row for the plot
-  fluidRow(
-    column(
-      width = 12, # The plot will occupy the full width of the page
-      # Wrap the plotOutput in a div with a fixed height and scrollable content
-      div(style = "overflow: auto;",
-          # This column will contain the plot output
-          plotOutput("heatmap_plot")
+    ),  # Closing parenthesis added here to close the div properly
+    br(),
+    br(),
+    hr(),
+    div(
+      class = "instruct",
+      p(
+        strong("Instructions:"),
+        br(),
+        "1. Select one or more TFs from the list.**",
+        br(),
+        "2. Click the 'Plot Heatmap' button to generate the visualization.",
+        br(),
+        br(),
+        strong("**We recommend visualizing less than 30 TFs at one time for readability."),
+        br(),
+        em("Note: The full dataset of activity scores can be found",
+           a(href = "https://github.com/lasseignelab/230323_JW_DiseaseNetworks/blob/main/results/tf_activity/tf_activity_scores.csv", "here", target = "_blank"))
+      )
+    ),
+    
+    # First fluid row for input elements
+    fluidRow(
+      column(
+        width = 4, # Adjust the width to your preference for input elements
+        # Input elements here (e.g., selectInput, actionButton, etc.)
+        selectInput("tf_input_select", "Select TF(s):", choices = all_tfs, multiple = TRUE),
+        actionButton("plot_button", "Plot Heatmap")
+      )
+    ),
+    
+    # Second fluid row for the plot
+    fluidRow(
+      column(
+        width = 12, # The plot will occupy the full width of the page
+        # Wrap the plotOutput in a div with a fixed height and scrollable content
+        div(style = "overflow: auto;",
+            # This column will contain the plot output
+            plotOutput("heatmap_plot")
+        )
       )
     )
   )
